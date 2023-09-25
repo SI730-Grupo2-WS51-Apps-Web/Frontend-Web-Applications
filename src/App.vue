d1629b7f5c483af2db66ace414359c359a5d1eff
Marpellic <marcelo.scerpella@gmail.com>
2023-09-25 01:08:57 -0500

    feat: Navbar created

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router';
+import navbarComponent from "@/public/components/navbar.component.vue";
+</script>
+
+<template>
+    <header>
+      <navbarComponent/>
+    </header>
+    <main>
+      <div class="wrapper">
+
+        <nav>
+          <RouterLink to="/">Home</RouterLink>
+          <RouterLink to="/about">About</RouterLink>
+        </nav>
+      </div>
+      <RouterView />
+    </main>
+    <footer>
+
+    </footer>
+
+
+</template>
+
+<style scoped>
+main{
+  margin: 10px;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,28 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+
+import PrimeVue from 'primevue/config';
+import 'primeicons/primeicons.css';
+import "@/public/styles/styles.scss"
+import Dialog from "primevue/dialog";
+import Button from "primevue/button";
+import SplitButton from "primevue/splitbutton";
+import Toolbar from "primevue/toolbar";
+import Divider from 'primevue/divider';
+import InputText from 'primevue/inputtext';
+import Image from "primevue/image";
+
+
+createApp(App)
+    .use(router)
+    .use(PrimeVue, {ripple:true})
+    .component('pv-dialog', Dialog)
+    .component('pv-button', Button)
+    .component('pv-split-button', SplitButton)
+    .component('pv-toolbar', Toolbar)
+    .component('pv-divider', Divider)
+    .component('pv-input-text',InputText)
+    .component('pv-image', Image)
+    .mount('#app')
+
