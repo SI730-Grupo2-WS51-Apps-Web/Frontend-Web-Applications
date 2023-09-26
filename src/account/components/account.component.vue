85459e011a820d1ec62a4ac539a7d3ce9011ec62
Nirvana Garcia <nirvagarciav@gmail.com>
2023-09-26 13:00:42 -0500

    feat: implemented big account viewer

--- src/account/components/account.component.vue
+++ src/account/components/account.component.vue
@@ -6,7 +6,8 @@ import {getUserImageByUser} from "@/shared/services/image.service";
 export default {
   methods: {
     getUserInfo(newUserInfo){
-      if(newUserInfo){
+      console.log(newUserInfo)
+      if(newUserInfo !== undefined && newUserInfo !== null && newUserInfo){
         this.userInfo = newUserInfo;
         if(this.userInfo.personal.genre){
           this.welcomeText = "Bienvenido"
@@ -14,10 +15,11 @@ export default {
         else{
           this.welcomeText = "Bienvenida"
         }
+        if(!this.isUserLogged) this.isUserLogged=true;
         this.updateImage();
       }
       else{
-        this.isOpen=false;
+        if(this.isUserLogged) this.isUserLogged=false;
       }
     },
     getMode(newDarkMode){
@@ -43,12 +45,13 @@ export default {
       isDarkMode: false,
       currentMode: "light",
       profilePhoto:"default.png",
-      isOpen: false,
+      isUserLogged: false,
       welcomeText: "Bienvenido",
     };
   },
   created() {
     this.userInfo = accountService.methods.watchUser(this.getUserInfo)
+    this.isUserLogged = !!this.userInfo;
     this.updateImage();
     stylesService.methods.watchDarkMode(this.getMode);
   },
@@ -61,23 +64,116 @@ export default {
       },
       immediate: true,
     },
-    isOpen:{
-      handler(newIsOpen, oldIsOpen){
-        if(!newIsOpen){
-          this.$emit("closed");
-        }
-      }
-    }
   }
 }
 </script>
 
 <template>
-  <div class="account flex flex-col">
-    <pv-avatar :image="`public/images/profile/${userInfo.image}.png`" class="mr-2" alt="Usuario.png" id="mini-profile-avatar"/>
+  <div class="account flex flex-col" v-if="this.isUserLogged">
+    <pv-avatar :image="`public/images/profile/${userInfo.image}.png`" class="mr-2" alt="Usuario.png" id="main-profile-avatar"/>
+    <div class="flex flex-row welcome-content text">
+      <div id="accountWelcomeText">{{welcomeText}},</div>
+      <div id="accountUserText">{{this.userInfo.personal.firstName}}</div>
+    </div>
+    <div class="little-margin"/>
+    <div class="flex flex-row gap-8">
+      <pv-card class="profile-data-card little-margin">
+        <template #title><div class="align-center text-4"> Información Personal: </div></template>
+        <template #content>
+          <div class="text-1 little-margin-width">
+            <div class="flex-row flex-justify-content gap-1">
+                <div class="main-text">Nombre:</div>
+                <div class="text">{{this.userInfo.personal.firstName + " " + this.userInfo.personal.lastName}}</div>
+            </div>
+            <div class="flex-row flex-justify-content gap-1">
+              <div class="main-text">Sexo:</div>
+              <div class="text">{{this.userInfo.personal.genre?"Masculino":"Femenino"}}</div>
+            </div>
+            <div class="flex-row flex-justify-content gap-1">
+              <div class="main-text">Telefono:</div>
+              <div class="text">{{this.userInfo.personal.phone}}</div>
+            </div>
+            <div class="flex-row flex-justify-content gap-1">
+              <div class="main-text">Correo:</div>
+              <div class="text">{{this.userInfo.login.email}}</div>
+            </div>
+            <div class="flex-row flex-justify-content gap-1">
+              <div class="main-text">Contraseña:</div>
+              <div class="text">{{this.userInfo.login.password}}</div>
+            </div>
+          </div>
+        </template>
+      </pv-card>
+      <pv-card class="profile-data-card little-margin">
+        <template #title><div class="align-center text-4"> Información de Compras: </div></template>
+        <template #content>
+          <div class="text-1 little-margin-width">
+            <div class="flex-row flex-justify-content gap-1">
+              <div class="main-text">Direccion:</div>
+              <div class="text">{{this.userInfo.shipping.address}}</div>
+            </div>
+            <div class="flex-row flex-justify-content gap-1">
+              <div class="main-text">Distrito:</div>
+              <div class="text">{{this.userInfo.shipping.district}}</div>
+            </div>
+            <div class="flex-row flex-justify-content gap-1">
+              <div class="main-text">Provincia:</div>
+              <div class="text">{{this.userInfo.shipping.province}}</div>
+            </div>
+            <div class="flex-row flex-justify-content gap-1">
+              <div class="main-text">Metodo de Pago:</div>
+              <div class="text">{{this.userInfo.payment.paymentMethod}}</div>
+            </div>
+            <div class="flex-row flex-justify-content gap-1">
+              <div class="main-text">{{this.userInfo.payment.paymentMethod}}:</div>
+              <div class="text">{{this.userInfo.payment.MethodData.name}}</div>
+            </div>
+          </div>
+        </template>
+      </pv-card>
+    </div>
+
+  </div>
+  <div class="account flex flex-col text" v-else>
+    <pv-avatar image="images/characters/astronaut_not_here.png" class="mr-2" alt="No deberías estar aquí..." id="not-logged-avatar"/>
+    <div>Hmp... Parece que no debería estar aquí todavía...</div>
+    <div>Quizá deberías tratar de <router-link to="login" id="link">iniciar sesión</router-link> primero</div>
   </div>
 </template>
 
-<style scoped>
-
+<style>
+.welcome-content{
+  align-items: flex-start;
+  align-content: flex-start;
+}
+#accountUserText{
+  padding-left: 1rem;
+  padding-top: 0.5rem;
+  color: var(--akira-red);
+  font-family: JAPANBRUSH,serif;
+  font-size: 3.2rem;
+}
+#accountWelcomeText{
+  font-size: 2.5rem;
+}
+#main-profile-avatar {
+  height: 20rem;
+  width: 20rem;
+  border-radius: 2rem;
+}
+#main-profile-avatar img{
+  height: 20rem;
+  width: 20rem;
+  border-radius: 2rem;
+}
+#not-logged-avatar {
+  height: 20rem;
+  width: 20rem;
+  border-radius: 2rem;
+}
+#not-logged-avatar img{
+  height: 20rem;
+  width: 20rem;
+  border-radius: 2rem;
+}
 </style>
--- src/account/services/account.service.js
+++ src/account/services/account.service.js
@@ -1,13 +1,41 @@
 import http from "@/shared/services/http-common";
 
+const paymentMethods={
+    card: 1,
+    wallet: 2,
+}
 async function searchUserByLoginData(email, password){
     try {
         const response = await http.get(`/users?login.email=${email}&login.password=${password}`);
         const userData = response.data;
-        console.log(userData);
         if(userData !== undefined){
             if(userData.length > 0){
-                console.log(userData[0])
+                userData[0].login.password = "***********";
+                const paymentMethodResponse = await http.get(`/paymentMethods?id=${userData[0].payment.selectedMethod}`);
+                const paymentData = paymentMethodResponse.data;
+                if(paymentData && paymentData.length === 1){
+                    userData[0].payment.paymentMethod = paymentData[0].name;
+                    switch (paymentData[0].id){
+                        case paymentMethods.card:
+                            userData[0].payment.MethodData = {};
+                            userData[0].payment.MethodData.name = userData[0].payment.card["number"];
+                            userData[0].payment.MethodData.maxCost = 100000;
+                            break;
+                        case paymentMethods.wallet:
+                            const walletResponse = await http.get(`/wallets?id=${userData[0].payment.wallet.id}`);
+                            const walletData = walletResponse.data;
+                            userData[0].payment.MethodData = {};
+                            userData[0].payment.MethodData.name = walletData[0].name;
+                            userData[0].payment.MethodData.maxCost = walletData[0].max;
+                            break;
+                    }
+                }
+                else {
+                    userData[0].payment.paymentMethod = "Undefined";
+                    userData[0].payment.MethodData.name = "Undefined";
+                    userData[0].payment.MethodData.maxCost = 0;
+                }
+                console.log("uwu", userData)
                 return userData[0];
             }
             else {
