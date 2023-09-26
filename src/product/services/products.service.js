457c69aaf069713a8da145c396bfc9299085609c
Marpellic <marcelo.scerpella@gmail.com>
2023-09-26 02:10:06 -0500

    feat: user menu implemented

--- src/product/services/products.service.js
+++ src/product/services/products.service.js
@@ -1,14 +1,16 @@
-import axios from "axios";
-
-// URL de tu servidor JSON
-const serverUrl = "http://localhost:3000";
-
+import http from "@/shared/services/http-common";
 
 // Función para obtener el atributo 'image' de un usuario por su ID
 async function getProductListLike(like, limit = 3) {
     try {
-        const response = await axios.get(`${serverUrl}/products?name_like=${like}&_limit=${limit}`);
-        return response.data;
+        const response = await http.get(`/products?name_like=${like}&_limit=${limit}`);
+        const products = response.data;
+        if(products){
+            return products;
+        }
+        else{
+            return undefined;
+        }
     } catch (error) {
         console.error("Error al solicitar datos de los productos en products.service.js", error);
         throw error;
