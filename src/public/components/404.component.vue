f455419c7fc438c5077c697fc7afea5b3278ad1f
Marpellic <marcelo.scerpella@gmail.com>
2023-09-26 12:57:28 -0500

    feat: Add default page for router

---
+++ src/public/components/404.component.vue
@@ -0,0 +1,28 @@
+<script >
+export default {
+
+}
+</script>
+
+<template>
+  <div class="account flex flex-col text">
+    <pv-avatar image="images/characters/astronaut_not_here.png" class="mr-2" alt="No deberías estar aquí..." id="not-found"/>
+    <div>Lo sentimos, la página que ha solicitado</div>
+    <div> no existe o no está disponible. Si deseas,</div>
+    <div>podemos llevarte a la <router-link to="/" id="link">página principal</router-link></div>
+
+  </div>
+</template>
+
+<style scoped>
+  #not-found{
+    height: 16rem;
+    width: 16em;
+    border-radius: 4rem;
+  }
+  #not-found img{
+    height: 16rem;
+    width: 16em;
+    border-radius: 4rem;
+  }
+</style>
