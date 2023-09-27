8f0fda9d3e1c2a29bebffc00517d16bfa1675796
Marpellic <marcelo.scerpella@gmail.com>
2023-09-27 15:49:11 -0500

    style fix: Fixed profile photo not being shown due to incorrect filename

--- src/account/components/profile-mini.component.vue
+++ src/account/components/profile-mini.component.vue
@@ -88,7 +88,7 @@ export default {
     <template #header></template>
     <template #default>
       <div class="flex flex-col">
-        <pv-avatar :image="`public/images/profile/${userInfo.image}.png`" class="mr-2" alt="Usuario.png" id="mini-profile-avatar"/>
+        <pv-avatar :image="`public/images/profile/${userInfo.image}`" class="mr-2" alt="Usuario.png" id="mini-profile-avatar"/>
         <div id="welcomeText" style="margin: 2px">{{welcomeText}}</div>
         <div id="userText" style="margin: 2px">{{this.userInfo.personal.firstName}}</div>
         <div class="flex flex-col align-left">
