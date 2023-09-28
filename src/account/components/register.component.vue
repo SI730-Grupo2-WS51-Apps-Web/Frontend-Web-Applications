7490891cc6b170a1e2ac26a4a87afe30ce4341e8
Prog-san <DieSanR@gmail.com>
2023-09-27 20:37:16 -0500

    feat: V1 Project implemented succesfully
    Co-authored-by: The Editor <marcelo.scerpella@gmail.com>
    Co-authored-by: Diego_Sanchez <u202021367@upc.edu.pe>
    Co-authored-by: Gabriel Bernal <u20201f415@upc.edu.pe>
    Co-authored-by: MichelleFMB <u202110543@ucp.edu.pe>

---
+++ src/account/components/register.component.vue
@@ -0,0 +1,37 @@
+<script>
+export default {
+  data(){
+    return{
+
+      loginCompleted: false,
+
+      newUser: {
+        id: Number.MAX_SAFE_INTEGER,
+        image: 0,
+        personal: {
+          firstName: "",
+          lastName: "",
+          phone: 0,
+        },
+        login: {},
+        email: "",
+        password: ""
+      },
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="flex flex-col login gap-1" v-if="newUser.loginCompleted === false">
+    <pv-input-text v-model="loginData.email" :class="{'p-invalid':emailFailed}" placeholder="correo"/>
+    <pv-password v-model="password" :class="{'p-invalid':passwordFailed}"  placeholder="contraseña" toggleMask :feedback="false"/>
+    <small class="p-error text" id="text-error">{{ errorMessage || '&nbsp;' }}</small>
+    <pv-button label="Iniciar Sesión" :severity="buttonStatus" size="large" rounded @click="tryLogin" :disabled="errorMessage"/>
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
